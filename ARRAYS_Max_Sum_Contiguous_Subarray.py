731fe61919a571ca0be91e57e63663cdab82bdf9
Ritesh Kumar Singh <46300590+RiteshSingh7@users.noreply.github.com>
2019-08-11 03:55:46 +0530

    Create ARRAYS_Max_Sum_Contiguous_Subarray.py

---
+++ ARRAYS_Max_Sum_Contiguous_Subarray.py
@@ -0,0 +1,12 @@
+class Solution:
+    # @param A : tuple of integers
+    # @return an integer
+    def maxSubArray(self, A):
+        maxx = -float('inf')
+        total = 0
+        for x in A:
+            total += x
+            maxx = max(total,maxx)
+            if total<0:
+                total = 0
+        return maxx
